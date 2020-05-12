d2df30184177d62bab764e533f21bd56b4bc7f15
james <jameshaller27@gmail.com>
2020-05-11 23:30:07 -0700

    Add utils/backtracker.py

---
+++ utils/backtracker.py
@@ -0,0 +1,39 @@
+from utils.collections import *
+
+
+class Configuration:
+    def get_children(self) -> list:
+        raise NotImplementedError('Method not implemented')
+
+    def is_valid(self) -> bool:
+        raise NotImplementedError('Method not implemented')
+
+    def is_goal(self) -> bool:
+        raise NotImplementedError('Method not implemented')
+
+
+class Backtracker:
+    BFS = Queue
+    DFS = Stack
+
+    def run(self, init_config: Configuration, search_coll: Collection, *, goal_halts = True) -> tuple:
+        if type(search_coll) == type:
+            search_coll = search_coll()
+
+        goals = []
+
+        search_coll.push(init_config)
+        while len(search_coll) > 0:
+            config = search_coll.pop()
+
+            if not config.is_valid():
+                continue
+
+            if config.is_goal():
+                goals.append(config)
+                if goal_halts:
+                    return tuple(goal_halts)
+            else:
+                search_coll.push_many(config.get_children())
+
+        return tuple(goals)

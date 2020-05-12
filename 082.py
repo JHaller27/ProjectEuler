4387ab7601ab11728ed2a73cd8f6bb218b546a50
james <jameshaller27@gmail.com>
2020-05-12 00:50:35 -0700

    Read init matrix config from file

--- 082.py
+++ 082.py
@@ -64,13 +64,13 @@ class Matrix(Configuration):
         return self._col == self._col_count - 1
 
 
-init = [
-    [131, 673, 234, 103, 18],
-    [201, 96, 342, 965, 150],
-    [630, 803, 746, 422, 111],
-    [537, 699, 497, 121, 956],
-    [805, 732, 524, 37, 331]
-]
+init = []
+with open('matrix.txt', 'r') as fin:
+    for line in fin:
+        init.append(list(map(int, line.split(','))))
+
+print(init)
+exit()
 
 backtracker = Backtracker()
 for ridx in range(len(init)):

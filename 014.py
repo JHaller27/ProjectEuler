94e781336f4fbe62fb5d4e0987c6f66af4525c2c
james <jameshaller27@gmail.com>
2020-05-19 00:22:38 -0700

    Add solution 014

--- 014.py
+++ 014.py
@@ -1,8 +1,11 @@
+import sys
 from utils.decorators import *
 
+sys.setrecursionlimit(10**6)
 
-@memoize
-def length(n):
+
+@memoize()
+def length(n) -> int:
     if n == 1:
         return 1
     if n % 2 == 0:
@@ -10,5 +13,4 @@ def length(n):
     return 1 + length(3 * n + 1)
 
 
-print(max(range(1, 1000000), key=length))
-
+print(max(range(1, 10**6), key=length))

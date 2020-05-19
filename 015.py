198fee0d2835465f9ad18bc9f971dd84d2101168
james <jameshaller27@gmail.com>
2020-05-19 01:06:50 -0700

    Improve solution 015

--- 015.py
+++ 015.py
@@ -7,8 +7,9 @@ def count_paths(size: int) -> int:
         grid[i][0] = grid[0][i] = 1
 
     for row in range(1, size):
-        for col in range(1, size):
-            grid[row][col] = grid[row][col-1] + grid[row-1][col]
+        for col in range(row, size):
+            # Grid is mirrored over x=y axis
+            grid[row][col] = grid[col][row] = grid[row][col-1] + grid[row-1][col]
 
     return grid[-1][-1]
 

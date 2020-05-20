bb584c654f37b0a9947c5b7b9672f5492b891ae4
james <jameshaller27@gmail.com>
2020-05-19 17:12:03 -0700

    Improve solution 022

--- p022.py
+++ p022.py
@@ -1,15 +1,4 @@
-def get_names(fname: str) -> list:
-    with open(fname, 'r') as fin:
-        return fin.read().replace('"', '').split(',')
-
-
-def score(name: str, line_num: int) -> int:
-    return line_num * sum(map(lambda c: ord(c) - ord('A') + 1, name))
-
-
-def main(names: list):
-    return sum(score(name, idx + 1) for idx, name in enumerate(sorted(names)))
-
-
-if __name__ == '__main__':
-    print(main(get_names('data/022.txt')))
+with open('data/022.txt', 'r') as fin:
+    print(sum((idx + 1) * sum(map(lambda c: ord(c) - ord('A') + 1, name))
+        for idx, name in enumerate(sorted(fin.read().replace('"', '').split(',')))
+    ))

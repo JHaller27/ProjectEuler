0d884babe10ec9bf567ee01a1ee89de87d71589d
james <jameshaller27@gmail.com>
2020-06-23 21:28:16 -0700

    Add moreitertools.itake_many()

--- utils/moreitertools.py
+++ utils/moreitertools.py
@@ -3,3 +3,14 @@ import itertools
 
 def itake(iterable, idx: int):
     return next(itertools.islice(iterable, idx, idx+1))
+
+
+def itake_many(iterable, idxs):
+    sorted_idxs = sorted(idxs)
+    ei = enumerate(iterable)
+
+    while len(sorted_idxs) > 0:
+        i, e = next(ei)
+        if i == sorted_idxs[0]:
+            yield e
+            sorted_idxs.pop(0)

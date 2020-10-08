42311ec5bebdc65d9f6485aab362bf203317c657
James Haller <jahaller@microsoft.com>
2020-10-08 16:06:30 -0700

    Add --push to solve.sh

--- solve.sh
+++ solve.sh
@@ -3,12 +3,14 @@ usage() {
     echo "    pnum         Problem number (eg 027)"
     echo "    -d|--dry     Dry run - show commands, but don't run them"
     echo "    -u|--update  Improve solution rather than 'solving it' for the first time"
+    echo "    -p|--push    Automatically push new commit"
     echo "    -h|--help    Show this help text"
 }
 
 update=false
 dry=false
 pnum=-1
+push=false
 
 while [[ $# -ne 0 ]]; do
     if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
@@ -18,6 +20,8 @@ while [[ $# -ne 0 ]]; do
         dry=true
     elif [[ "$1" == "-u" ]] || [[ "$1" == "--update" ]]; then
         update=true
+    elif [[ "$1" == "-p" ]] || [[ "$1" == "--push" ]]; then
+        push=true
     else
         pnum="$1"
     fi
@@ -30,20 +34,29 @@ if [[ pnum -eq -1 ]]; then
     exit 1
 fi
 
+# git add
+fname="p$pnum.py"
+cmds=("git add \"$fname\"")
+
+# git commit
 if $update; then
     commit_msg="Improve"
 else
     commit_msg="Solve"
 fi
 commit_msg="$commit_msg problem $pnum"
+cmds=("${cmds[@]}", "git commit -m \"$commit_msg\"")
 
-fname="p$pnum.py"
-
-if $dry; then
-    echo "git add \"$fname\""
-    echo "git commit -m \"$commit_msg\""
-else
-    git add $fname
-    git commit -m "$commit_msg"
+# git push
+if $push; then
+    cmds=("${cmds[@]}", "git push")
 fi
+
+for c in "${cmds[@]}"; do
+    if $dry; then
+        echo "$c"
+    else
+        "$c"
+    fi
+done
 
